af607b59d4295de138887b2a6ba27ce3792e0da9
Jacob Jorgensen <17Jorgensenj@badinhs.org>
2016-10-07 08:02:54 -0400

    Baden theme

--- Badin theme/ViewController.swift
+++ Badin theme/ViewController.swift
@@ -8,17 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var Tablepeople: UITableView!
+    
+    var SpecialEvents = ["Block Days","Sports","Off School Days","Meetings","Trips"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Tablepeople.dataSource = self
+        
+        Tablepeople.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SpecialEvents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = SpecialEvents[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "Move", sender: "🙄")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
+   
+  
 
 
 }
