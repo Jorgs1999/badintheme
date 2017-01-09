a64de9ab376c59ae5168760775c201304fe010bc
Jacob Jorgensen <17Jorgensenj@badinhs.org>
2017-01-09 08:00:05 -0500

    Final thing

---
+++ Badin theme/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Badin theme
+//
+//  Created by BHSRam4 on 1/8/17.
+//  Copyright © 2017 BHSRam4. All rights reserved.
+//
+
+import Foundation
--- Badin theme/ViewController.swift
+++ Badin theme/ViewController.swift
@@ -2,41 +2,30 @@
 //  ViewController.swift
 //  Badin theme
 //
-//  Created by BHSRam4 on 10/6/16.
+//  Created by BHSRam4 on 1/1/17.
 //  Copyright © 2016 BHSRam4. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController{
     
-    @IBOutlet weak var Tablepeople: UITableView!
     
-    var SpecialEvents = ["Block Days","Sports","Off School Days","Meetings","Trips","Mass days","out of uniform days","concerts","retreats","Service"]
     
+    @IBAction func button(_ sender: AnyObject) {
+        self.performSegue(withIdentifier: "Move", sender: nil)
+        print("hi")
+    }
 
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        Tablepeople.dataSource = self
-        
-        Tablepeople.delegate = self
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return SpecialEvents.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print(indexPath.row)
-        let cell = UITableViewCell()
-        cell.textLabel?.text = SpecialEvents[indexPath.row]
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "Move", sender: "🙄")
+       
+       
     }
 
     override func didReceiveMemoryWarning() {
